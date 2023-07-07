42a6dbc9b48bb1f4acd4d2d0365314ab16437bda
LaurieLecomte <laurieblecomte@gmail.com>
2023-07-07 10:53:15 -0400

    improved F_MISS plotting script

--- 01_scripts/utils/compute_DP_FMISS_stats.R
+++ 01_scripts/utils/compute_DP_FMISS_stats.R
@@ -6,6 +6,8 @@ RAW_STATS <- '/mnt/ibis/lbernatchez/users/lalec31/RDC_Romaine/03_SR_LR/genotype_
 FILT_STATS <- '/mnt/ibis/lbernatchez/users/lalec31/RDC_Romaine/03_SR_LR/genotype_SVs_SRLR/09_filtered/merged_SUPP2_MAF0.05_FMISS0.5_DP_FMISS.table'
 
 library(data.table)
+library(dplyr)
+library(ggplot2)
 
 raw_stats <- fread(RAW_STATS, col.names = c('CHROM', 'POS', 'DP', 'F_MISSING'))
 filt_stats <- fread(FILT_STATS, col.names = c('CHROM', 'POS', 'DP', 'F_MISSING'))
@@ -30,3 +32,35 @@ variants_stats(raw_stats$F_MISSING)
 # On filtered data
 variants_stats(filt_stats$DP)
 variants_stats(filt_stats$F_MISSING)
+
+
+# 3. Explore F_MISSING ----------------------------------------------------
+# Split F_MISSING into equal size bins
+raw_stats$F_MISS_bins <- cut_interval(raw_stats$F_MISSING, 
+                                    length = 0.1, right = FALSE)
+
+# Assign 'kept' or 'filtered out' tag to each SV
+raw_stats$F_MISS_groups <-
+sapply(X = raw_stats$F_MISS_bins,
+       FUN = function(x){
+         ifelse(x %in% levels(raw_stats$F_MISS_bins)[1:5], 
+                yes = 'passed',
+                no = 'failed')
+       })
+
+
+# Plot
+SVs_F_MISS_plot <- 
+ggplot(data = raw_stats) +
+  geom_bar(aes(F_MISS_bins, fill = F_MISS_groups)) + 
+  theme(
+    axis.text.x = element_text(angle = 45, size = 8, hjust = 1)
+  ) + scale_fill_manual(values = c('red', 'grey60')) +
+  labs(x = 'Proportion of missing genotypes',
+       y = 'SV count',
+       fill = 'F_MISSING filter')
+
+saveRDS(SVs_F_MISS_plot,
+        file = '/mnt/ibis/lbernatchez/users/lalec31/RDC_Romaine/03_SR_LR/genotype_SVs_SRLR/09_filtered/merged_SUPP2_MAF0.05_FMISS0.5_DP_FMISS_FMISSplot.rds')
+
+
